e2242cdf88ee6cbc6368a981271596fb9059e7a5
Marek Fajkus <marek.faj@gmail.com>
2014-07-24 03:50:32 +0200

    basic tasks setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,67 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
 		sass: {
-
+			dev: {
+				files: [{
+					expand: true,
+					cwd: 'src/sass',
+					src: ['*.{scss, sass}'],
+					dest: 'dist/css',
+					ext: '.css'
+				}]
+			},
+			dist: {
+				files: [{
+					expand: true,
+					cwd: 'src/sass',
+					src: ['*.{scss, sass}'],
+					dest: 'dist/css',
+					ext: '.css'
+				}],
+				options: {
+					style: 'compressed'
+				}
+			}
 		},
 		jshint: {
-
+			scripts: ['src/javascript/jquery.babylongrid.js'],
+			gruntfile: ['Gruntfile.js']
 		},
 		uglify: {
-
+			scripts: {
+				files: [{
+				expand: true,
+				cwd: 'src/javascript/',
+				src: '*.js',
+				dest: 'dist/js'
+			}]
+			}
 		},
 		connect: {
-
+			server: {
+				options: {
+					hostname: '127.0.0.1',
+					port: 1337,
+					base: 'demo'
+				}
+			}
 		},
 		watch: {
-
+			scripts: {
+				files: 'src/javascript/jquery.babylongrid.js',
+				tasks: ['jshint:scripts'],
+			},
+			sass: {
+				files: 'src/sass/*.{sass, scss}',
+				tasks: ['sass:dev']
+			},
+			statics: {
+				files: 'demo/index.html',
+				tasks: []
+			},
+			options: {
+				livereload: true
+			}
 		}
 	});
 
@@ -23,9 +71,9 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-connect');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
-	grunt.registerTask('dev', []);
-	grunt.registerTask('serve', []);
+	grunt.registerTask('dev', ['jshint', 'sass:dev']);
+	grunt.registerTask('serve', ['dev', 'connect:server', 'watch']);
 	grunt.registerTask('server', ['serve']);
-	grunt.registerTask('dist', []);
+	grunt.registerTask('dist', ['jshint', 'sass:dist', 'uglify:scripts']);
 	grunt.registerTask('default', ['dist']);
 };
